41908c70a92715bcb5e9c7ff25bf7725be0a9ae8
im-naren <narendra.kumar@razorpay.com>
2025-01-16 14:04:51 +0530

    update

--- requests.go
+++ requests.go
@@ -7,19 +7,30 @@ import (
 	"log"
 )
 
+func buildGetURL(url string, params map[string]string) string {
+    queryString := ""
+    for k, v := range params {
+        queryString += fmt.Sprintf("%s=%s&", k, v)
+    }
+    queryString = queryString[:len(queryString)-1]
+    return fmt.Sprintf("%s?%s", url, queryString)
+}
 
-func get(url string) string {
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	responseData, err := io.ReadAll(response.Body)
+func get(url string, params map[string]string) string {
+    _url := buildGetURL(url, params)
+    response, err := http.Get(_url)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return ""
+    }
+    defer response.Body.Close()
+    responseData, err := io.ReadAll(response.Body)
     if err != nil {
         log.Fatal(err)
     }
-	return string(responseData)
+    return string(responseData)
 }
 
 func main() {
-	fmt.Println(get("https://www.google.com"))
+    fmt.Println(get("https://www.google.com", map[string]string{}))
 }
